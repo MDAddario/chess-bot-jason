3a547f8016bd4249af5c4ac961681b1fab31d28f
MDAddario <michael.lindner.daddario@gmail.com>
2020-05-05 12:26:43 -0400

    Creates helper functions to improve Chessboard interfacing

--- src/representation/Chessboard.java
+++ src/representation/Chessboard.java
@@ -3,6 +3,7 @@ package representation;
 class Chessboard {
 
     // Distinguish the piece colors and piece types
+    private static final int NONE   = -1;
     private static final int BLACK  = Piece.BLACK;
     private static final int WHITE  = Piece.WHITE;
     private static final int KING   = Piece.KING;
@@ -31,49 +32,90 @@ class Chessboard {
         this.setDefaultBoards();
     }
 
+    // Protection methods
+    private boolean isTypeLegal(int type)   { return 2 <= type && type < 8; }
+    private boolean isColorLegal(int color) { return 0 <= color && color < 2; }
+
+    // Helper function for managing boards
+    private void setBit(char file, int rank, int type, int color, boolean isActive) {
+
+        // Validate boards input
+        if (!isTypeLegal(type))
+            throw new IllegalArgumentException("Piece type specified is illegal.");
+        if (!isColorLegal(color))
+            throw new IllegalArgumentException("Color specified is illegal.");
+
+        // Set the boards
+        this.boards[color].setBit(file, rank, isActive);
+        this.boards[type] .setBit(file, rank, isActive);
+    }
+
+    // More helper functions
+    private int getColor(char file, int rank) {
+
+        if (this.boards[WHITE].getBit(file, rank))
+            return WHITE;
+        else if (this.boards[BLACK].getBit(file, rank))
+            return BLACK;
+        return NONE;
+    }
+
+    private int getType(char file, int rank) {
+
+        for (int type = 2; type < 8; type++)
+            if (this.boards[type].getBit(file, rank))
+                return type;
+        return NONE;
+    }
+
     // Load starting chess position
     private void setDefaultBoards() {
 
-        for (char file = 'A'; file <= 'H'; file++) {
-
-            // White color board
-            this.boards[WHITE].setBit(file, 1, true);
-            this.boards[WHITE].setBit(file, 2, true);
-
-            // Black color board
-            this.boards[BLACK].setBit(file, 7, true);
-            this.boards[BLACK].setBit(file, 8, true);
-
-            // Pawns
-            this.boards[PAWN].setBit(file, 2, true);
-            this.boards[PAWN].setBit(file, 7, true);
-        }
+        // White frontend
+        this.setBit('A', 2, PAWN, WHITE, true);
+        this.setBit('B', 2, PAWN, WHITE, true);
+        this.setBit('C', 2, PAWN, WHITE, true);
+        this.setBit('D', 2, PAWN, WHITE, true);
+        this.setBit('E', 2, PAWN, WHITE, true);
+        this.setBit('F', 2, PAWN, WHITE, true);
+        this.setBit('G', 2, PAWN, WHITE, true);
+        this.setBit('H', 2, PAWN, WHITE, true);
 
         // White backend
-        this.boards[ROOK]  .setBit('A', 1, true);
-        this.boards[KNIGHT].setBit('B', 1, true);
-        this.boards[BISHOP].setBit('C', 1, true);
-        this.boards[QUEEN] .setBit('D', 1, true);
-        this.boards[KING]  .setBit('E', 1, true);
-        this.boards[BISHOP].setBit('F', 1, true);
-        this.boards[KNIGHT].setBit('G', 1, true);
-        this.boards[ROOK]  .setBit('H', 1, true);
+        this.setBit('A', 1, ROOK,   WHITE, true);
+        this.setBit('B', 1, KNIGHT, WHITE, true);
+        this.setBit('C', 1, BISHOP, WHITE, true);
+        this.setBit('D', 1, QUEEN,  WHITE, true);
+        this.setBit('E', 1, KING,   WHITE, true);
+        this.setBit('F', 1, BISHOP, WHITE, true);
+        this.setBit('G', 1, KNIGHT, WHITE, true);
+        this.setBit('H', 1, ROOK,   WHITE, true);
+
+        // Black frontend
+        this.setBit('A', 7, PAWN, BLACK, true);
+        this.setBit('B', 7, PAWN, BLACK, true);
+        this.setBit('C', 7, PAWN, BLACK, true);
+        this.setBit('D', 7, PAWN, BLACK, true);
+        this.setBit('E', 7, PAWN, BLACK, true);
+        this.setBit('F', 7, PAWN, BLACK, true);
+        this.setBit('G', 7, PAWN, BLACK, true);
+        this.setBit('H', 7, PAWN, BLACK, true);
 
         // Black backend
-        this.boards[ROOK]  .setBit('A', 8, true);
-        this.boards[KNIGHT].setBit('B', 8, true);
-        this.boards[BISHOP].setBit('C', 8, true);
-        this.boards[QUEEN] .setBit('D', 8, true);
-        this.boards[KING]  .setBit('E', 8, true);
-        this.boards[BISHOP].setBit('F', 8, true);
-        this.boards[KNIGHT].setBit('G', 8, true);
-        this.boards[ROOK]  .setBit('H', 8, true);
+        this.setBit('A', 8, ROOK,   BLACK, true);
+        this.setBit('B', 8, KNIGHT, BLACK, true);
+        this.setBit('C', 8, BISHOP, BLACK, true);
+        this.setBit('D', 8, QUEEN,  BLACK, true);
+        this.setBit('E', 8, KING,   BLACK, true);
+        this.setBit('F', 8, BISHOP, BLACK, true);
+        this.setBit('G', 8, KNIGHT, BLACK, true);
+        this.setBit('H', 8, ROOK,   BLACK, true);
     }
 
     @Override
     public String toString() {
 
-        // Create correspondence array
+        // Create correspondence arrays
         char[] names = new char[8];
         names[KING]   = 'K';
         names[QUEEN]  = 'Q';
@@ -82,28 +124,25 @@ class Chessboard {
         names[KNIGHT] = 'N';
         names[PAWN]   = 'P';
 
+        char[] colors = new char[2];
+        colors[WHITE] = 'w';
+        colors[BLACK] = 'b';
+
         StringBuilder output = new StringBuilder();
         output.append("-----".repeat(8));
         output.append("\n");
         for (int rank = 8; rank >= 1; rank--) {
             for (char file = 'A'; file <= 'H'; file++) {
 
-                char color = '\0';
+                int color = this.getColor(file, rank);
+                int type  = this.getType(file, rank);
 
-                if (this.boards[WHITE].getBit(file, rank))
-                    color = 'w';
-                else if (this.boards[BLACK].getBit(file, rank))
-                    color = 'b';
-
-                if (color != '\0') {
-                    for (int type = 2; type < 8; type++)
-                        if (this.boards[type].getBit(file, rank)) {
-                            output.append(" ");
-                            output.append(color);
-                            output.append(names[type]);
-                            output.append(color);
-                            output.append(" ");
-                        }
+                if (color != NONE && type != NONE) {
+                    output.append(" ");
+                    output.append(colors[color]);
+                    output.append(names[type]);
+                    output.append(colors[color]);
+                    output.append(" ");
                 } else
                     output.append("  .  ");
             }
