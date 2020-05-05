2efe6855339c8060301f88d4c68501188c62da14
MDAddario <michael.lindner.daddario@gmail.com>
2020-05-05 13:51:05 -0400

    Conducts unit testing for Special Flags

--- src/representation/SpecialFlags.java
+++ src/representation/SpecialFlags.java
@@ -52,4 +52,70 @@ class SpecialFlags {
     void lowerWhiteLong()  { this.whiteLong  = false; }
     void lowerBlackShort() { this.blackShort = false; }
     void lowerBlackLong()  { this.blackLong  = false; }
+
+    // Compound setters
+    void lowerWhiteCastles() { this.lowerWhiteShort(); this.lowerWhiteLong(); }
+    void lowerBlackCastles() { this.lowerBlackShort(); this.lowerBlackLong(); }
+
+    // Unit testing
+    public static void main(String[] args) {
+
+        // Create new object
+        SpecialFlags flags = new SpecialFlags();
+
+        // Ensure no enpassant
+        for (char file = 'A'; file <= 'H'; file++)
+            if (flags.canEnPassant(file))
+                throw new UnitTestException("Constructor is wrong.");
+
+        // Ensure all castles
+        if (!flags.canWhiteShort())
+            throw new UnitTestException("Constructor is wrong.");
+        if (!flags.canWhiteLong())
+            throw new UnitTestException("Constructor is wrong.");
+        if (!flags.canBlackShort())
+            throw new UnitTestException("Constructor is wrong.");
+        if (!flags.canBlackLong())
+            throw new UnitTestException("Constructor is wrong.");
+
+        // Check independence of en passant flags
+        for (char focusFile = 'A'; focusFile <= 'H'; focusFile++) {
+
+            // Raise flag
+            flags.raiseEnPassant(focusFile);
+
+            // Check flag raised
+            if (!flags.canEnPassant(focusFile))
+                throw new UnitTestException("EP is wrong.");
+
+            // Check lowered for all other flags
+            for (char otherFile = 'A'; otherFile <= 'H'; otherFile++) {
+
+                // Ignore focus flag
+                if (focusFile == otherFile)
+                    continue;
+
+                // Check flags lowered
+                if (flags.canEnPassant(otherFile))
+                    throw new UnitTestException("EP is wrong.");
+            }
+
+            // Lower the flags
+            flags.lowerEnPassant();
+        }
+
+        // Check independence of castling rights
+        flags.lowerWhiteShort();
+        if (!flags.canWhiteLong() || !flags.canBlackShort() || !flags.canBlackLong())
+            throw new UnitTestException("Castling is wrong.");
+        flags.lowerWhiteLong();
+        if (!flags.canBlackShort() || !flags.canBlackLong())
+            throw new UnitTestException("Castling is wrong.");
+        flags.lowerBlackShort();
+        if (!flags.canBlackLong())
+            throw new UnitTestException("Castling is wrong.");
+        flags.lowerBlackLong();
+        if (flags.canWhiteShort() || flags.canWhiteLong() || flags.canBlackShort() || flags.canBlackLong())
+            throw new UnitTestException("Castling is wrong.");
+    }
 }
